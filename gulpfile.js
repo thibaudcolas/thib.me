34373026a05e460a1db80308c11720ef02e741c8
Thibaud Colas <thibaudcolas@gmail.com>
2014-03-12 23:44:28 +0100

    Add default dependencies managers + task to serve the website

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var open = require('open');
 var express = require('express');
 
 var app = express();
@@ -6,10 +7,16 @@ var app = express();
 var APP_ROOT = __dirname + '/source';
 var APP_PORT = 4000;
 
+// Opens a browser with the application path.
+gulp.task('open', ['serve'], function() {
+  open('http://localhost:' + APP_PORT);
+});
+
 // Statically serves files.
 gulp.task('serve', function() {
   app.use(express.static(APP_ROOT));
   app.listen(APP_PORT);
+  console.log('Listening on port ' + APP_PORT);
 });
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['open']);
