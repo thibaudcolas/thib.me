1d6ac5cc6775e4453cb007d9c6c04f2c8ae99c49
Thibaud Colas <thibaudcolas@gmail.com>
2014-03-19 00:55:33 +0100

    Add lint task to build file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var open = require('open');
 var express = require('express');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 
 var app = express();
 
@@ -17,6 +19,13 @@ gulp.task('serve', function() {
   app.use(express.static(APP_ROOT));
   app.listen(APP_PORT);
   console.log('Listening on port ' + APP_PORT);
+});
+
+// Asserts code quality with various static code analysis tools.
+gulp.task('lint', function() {
+  return gulp.src('source/js/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter(stylish));
 });
 
 gulp.task('default', ['open']);
