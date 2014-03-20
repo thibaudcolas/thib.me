a9742d8d3ab69b0c7ca7f0fada22e6f05cc34c26
Thibaud Colas <thibaudcolas@gmail.com>
2014-03-20 01:38:55 +0100

    Add first version of deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+if [ "$TRAVIS_REPO_SLUG" == "ThibWeb/thibaudcolas" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  echo -e "Deploying to GitHub Pages...\n"
+
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+
+  cd build
+  git init .
+  git add -f .
+  git commit -m "Lastest deploy on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+  git push -fq "https://${GH_TOKEN}@github.com/ThibWeb/thibaudcolas" "gh-pages" > /dev/null
+
+  echo -e "Finished deploy to GitHub Pages...\n"
+
+fi
