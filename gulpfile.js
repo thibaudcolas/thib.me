6e0d54fca055b7213de59e6a1aa02900600ada97
Thibaud Colas <thibaudcolas@gmail.com>
2014-06-24 07:50:21 +0200

    Add watch task w/ gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
+var path = require('path');
 var open = require('open');
+var lr = require('tiny-lr');
 var express = require('express');
 var es = require('event-stream');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var deploy = require('gulp-gh-pages');
 
+var server = lr();
 var app = express();
 
 var APP_ROOT = __dirname + '/source';
@@ -16,11 +19,32 @@ gulp.task('open', ['serve'], function() {
   open('http://localhost:' + APP_PORT);
 });
 
-// Statically serves files.
+// Statically serves files and adds the LiveReload script.
 gulp.task('serve', function() {
+  app.use(require('connect-livereload')());
   app.use(express.static(APP_ROOT));
   app.listen(APP_PORT);
-  console.log('Listening on port ' + APP_PORT);
+});
+
+// Watches for file changes and reloads browser pages.
+gulp.task('watch', function() {
+  server.listen(35729, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+
+    gulp.watch([
+      'source/index.html',
+      'source/assets/**/*',
+      'source/js/**/*'
+      ], function(evt) {
+        server.changed({
+          body: {
+            files: [path.relative(APP_ROOT, evt.path)]
+          }
+        });
+      });
+  });
 });
 
 // Builds the project before deployment.
@@ -45,5 +69,8 @@ gulp.task('deploy', function () {
     gulp.src('build/**/*')
         .pipe(deploy('git@github.com:ThibWeb/thibaudcolas.git', 'origin'));
 });
+
+// Default developer working task.
+gulp.task('work', ['watch', 'open']);
 
 gulp.task('default', ['open']);
