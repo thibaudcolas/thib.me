c1063d56d3e14feef6946ab4908d2a469add9783
Thibaud Colas <thibaudcolas@gmail.com>
2014-08-08 01:25:43 +0200

    add old JS mail hiding

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,19 @@
 (function () {
   'use strict';
 
+  var decode = function(arr) {
+    return arr.reduce((function(acc, car, i) {
+      if (acc == null) {
+        acc = '';
+      }
+      return acc + String.fromCharCode(car - i);
+    }), '');
+  };
+  var spam_txt = [116, 105, 107, 101, 68, 121, 110, 112, 106, 55, 119, 112];
+  var correctMail = decode(spam_txt);
+  console.log(correctMail);
+  // $('a.remplacement').attr('href', "mailto:" + correctMail);
+  // $('span.remplacement').html(correctMail);
+
+
 })();
