bc4d624389902510254317428774bf2fbf594e7a
Thibaud Colas <thibaudcolas@gmail.com>
2014-10-14 09:14:22 +0200

    Move ga tracking code to external file

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,10 @@
 (function () {
   'use strict';
 
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create','UA-34692999-4','thibaudcolas.fr');
+  ga('send','pageview');
+
   function decode (arr) {
     return arr.reduce(function(acc, car, i) {
       return acc + String.fromCharCode(car - i);
