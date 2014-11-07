11e820fb584d6e37dadd6ce9e766d01e0bc8044a
Thibaud Colas <thibaudcolas@gmail.com>
2014-11-07 02:05:54 +0100

    Revert "Add loadCSS to project"
    This reverts commit ab2a9cd129990c5989637653bddad86df1f6da1d.

--- gulpfile.js
+++ gulpfile.js
@@ -105,10 +105,8 @@ gulp.task('html', function() {
     .pipe(assets.restore())
     .pipe($.useref())
     // Minify Any HTML
-    .pipe($.if('*.html', $.replace('<script src', '<script async defer src')))
-    .pipe($.if('*.html', $.replace('<link rel="stylesheet" href="styles/main.min.css">', '<noscript><link rel="stylesheet" href="styles/main.min.css"></noscript>')))
-    .pipe($.if('*.html', $.replace('// build:', '')))
     .pipe($.if('*.html', $.minifyHtml()))
+    .pipe($.if('*.html', $.replace('<script src', '<script async defer src')))
     // Output Files
     .pipe(gulp.dest('dist'))
     .pipe($.size({title: 'html'}));
