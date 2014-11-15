6d58a47deb2268f6aabdccb56f46b5d75a61eb12
Thibaud Colas <thibaudcolas@gmail.com>
2014-11-15 11:16:47 +0100

    Implement encode

--- app/scripts/obfuscate.js
+++ app/scripts/obfuscate.js
@@ -2,11 +2,11 @@ export default {
   encode(str) {
     'use strict';
 
-    return [];
+    return str.split('').map((s, i, arr) => s.charCodeAt(0) + i);
   },
   decode(arr) {
     'use strict';
 
-    return arr.reduce((acc, char, i) => acc + String.fromCharCode(char - i), '');
+    return arr.reduce((acc, s, i) => acc + String.fromCharCode(s - i), '');
   }
 };
--- test/testObfuscate.js
+++ test/testObfuscate.js
@@ -6,6 +6,14 @@ describe('the encode function', function() {
   it('outputs nothing if nothing is entered', function () {
     expect(encode('')).toEqual([]);
   });
+
+  it('converts strings to integers', function () {
+    expect(encode('a')).toEqual([97]);
+  });
+
+  it('can hide my mail address', function() {
+    expect(encode('thib@thib')).toEqual([116, 105, 107, 101, 68, 121, 110, 112, 106]);
+  });
 });
 
 describe('the decode function', function() {
