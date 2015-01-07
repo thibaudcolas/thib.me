357053586cd5a1b316c773eca5ff542640697e36
Thibaud Colas <thibaudcolas@gmail.com>
2015-01-08 00:35:28 +1300

    Add Coast To Coast Walk w/ Leaflet, GeoJSON & OSM

---
+++ coast2coast-walk/script.js
@@ -0,0 +1,76 @@
+(function (L, C2CData) {
+  'use strict';
+
+  var map = L.map('map').setView([-36.84229705685982, 174.76312637329102], 13);
+
+  L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+      '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    id: 'examples.map-20v6611k'
+  }).addTo(map);
+
+  L.geoJson(C2CData).addTo(map);
+
+  // var baseballIcon = L.icon({
+  //   iconUrl: 'baseball-marker.png',
+  //   iconSize: [32, 37],
+  //   iconAnchor: [16, 37],
+  //   popupAnchor: [0, -28]
+  // });
+
+  // function onEachFeature(feature, layer) {
+  //   var popupContent = "<p>I started out as a GeoJSON " +
+  //       feature.geometry.type + ", but now I'm a Leaflet vector!</p>";
+
+  //   if (feature.properties && feature.properties.popupContent) {
+  //     popupContent += feature.properties.popupContent;
+  //   }
+
+  //   layer.bindPopup(popupContent);
+  // }
+
+  // L.geoJson([bicycleRental, campus], {
+
+  //   style: function (feature) {
+  //     return feature.properties && feature.properties.style;
+  //   },
+
+  //   onEachFeature: onEachFeature,
+
+  //   pointToLayer: function (feature, latlng) {
+  //     return L.circleMarker(latlng, {
+  //       radius: 8,
+  //       fillColor: "#ff7800",
+  //       color: "#000",
+  //       weight: 1,
+  //       opacity: 1,
+  //       fillOpacity: 0.8
+  //     });
+  //   }
+  // }).addTo(map);
+
+  // L.geoJson(freeBus, {
+
+  //   filter: function (feature, layer) {
+  //     if (feature.properties) {
+  //       // If the property "underConstruction" exists and is true, return false (don't render features under construction)
+  //       return feature.properties.underConstruction !== undefined ? !feature.properties.underConstruction : true;
+  //     }
+  //     return false;
+  //   },
+
+  //   onEachFeature: onEachFeature
+  // }).addTo(map);
+
+  // var coorsLayer = L.geoJson(coorsField, {
+
+  //   pointToLayer: function (feature, latlng) {
+  //     return L.marker(latlng, {icon: baseballIcon});
+  //   },
+
+  //   onEachFeature: onEachFeature
+  // }).addTo(map);
+
+})(window.L, window.C2CData);
