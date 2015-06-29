ab9eeb3372520661a0cb8697b5b063e15bd81d3d
Thibaud Colas <thibaudcolas@gmail.com>
2015-06-29 23:19:39 +1200

    Add second Metricsgraphics example

--- metricsgraphics-sparkline/script.js
+++ metricsgraphics-sparkline/script.js
@@ -1,7 +1,51 @@
 (function(MG) {
     'use strict';
 
-    var data = [
+    var data2 = [
+        { year: new Date('1991-01-01'), arrivals: 50578, departures: 44181},
+        { year: new Date('1992-01-01'), arrivals: 48144, departures: 43504},
+        { year: new Date('1993-01-01'), arrivals: 55081, departures: 41096},
+        { year: new Date('1994-01-01'), arrivals: 64382, departures: 44525},
+        { year: new Date('1995-01-01'), arrivals: 77563, departures: 49077},
+        { year: new Date('1996-01-01'), arrivals: 79005, departures: 54212},
+        { year: new Date('1997-01-01'), arrivals: 67636, departures: 60012},
+        { year: new Date('1998-01-01'), arrivals: 58220, departures: 64485},
+        { year: new Date('1999-01-01'), arrivals: 59743, departures: 68772},
+        { year: new Date('2000-01-01'), arrivals: 62994, departures: 74306},
+        { year: new Date('2001-01-01'), arrivals: 81094, departures: 71368},
+        { year: new Date('2002-01-01'), arrivals: 95951, departures: 57753},
+        { year: new Date('2003-01-01'), arrivals: 92660, departures: 57754},
+        { year: new Date('2004-01-01'), arrivals: 80479, departures: 65371},
+        { year: new Date('2005-01-01'), arrivals: 78963, departures: 71992},
+        { year: new Date('2006-01-01'), arrivals: 82732, departures: 68123},
+        { year: new Date('2007-01-01'), arrivals: 82572, departures: 77081},
+        { year: new Date('2008-01-01'), arrivals: 87463, departures: 83649},
+        { year: new Date('2009-01-01'), arrivals: 86410, departures: 65157},
+        { year: new Date('2010-01-01'), arrivals: 82469, departures: 72018},
+        { year: new Date('2011-01-01'), arrivals: 84187, departures: 86042},
+        { year: new Date('2012-01-01'), arrivals: 85255, departures: 86420},
+        { year: new Date('2013-01-01'), arrivals: 93965, departures: 71497},
+        { year: new Date('2014-01-01'), arrivals: 109317, departures: 58395}
+    ];
+
+    MG.data_graphic({
+        title: "Migrant arrivals and departures",
+        description: "",
+        data: data2,
+        full_width: true,
+        height: 150,
+        area: false,
+        y_axis: false,
+        x_axis: false,
+        target: '#chart2',
+        x_accessor: 'year',
+        y_accessor: ['arrivals', 'departures'],
+        markers: [],
+        legend: ['Arrivals', 'Departures'],
+        legend_target: '#legend2'
+    });
+
+    var data1 = [
         { year: new Date('1991-01-01'), value: 9152},
         { year: new Date('1992-01-01'), value: 9114},
         { year: new Date('1993-01-01'), value: 9193},
@@ -31,18 +75,16 @@
     MG.data_graphic({
         title: "Divorces",
         description: "Total number of divorces, yearly",
-        data: data,
-        width: 650,
+        data: data1,
+        full_width: true,
         height: 150,
         area: false,
         y_axis: false,
         x_axis: false,
-        target: '#chart',
+        target: '#chart1',
         x_accessor: 'year',
         y_accessor: 'value',
-        markers: [
-            {'year': 1964, 'label': '"The Creeping Terror" released'}
-        ]
+        markers: []
     });
 
 })(window.MG);
