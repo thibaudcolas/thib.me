7bc86399e1ebe6721ad9022a56a255a0b1e42973
Thibaud Colas <thibaudcolas@gmail.com>
2015-07-06 23:00:14 +1200

    Add another MetricsGraphics chart

--- international-flights-nz/script.js
+++ international-flights-nz/script.js
@@ -51,6 +51,43 @@
                 y_accessor: 'TOTAL ALL OVERSEAS PORTS',
                 markers: []
             });
+
+            var monthToMonthChange = data.map(function (d, i) {
+                var ret = {};
+
+                var lastIndex = i === 0 ? 0 : i - 1;
+                var lastMonth = data[lastIndex];
+
+                Object.keys(d).forEach(function(key) {
+                    var value = d[key];
+                    var lastValue = lastMonth[key];
+
+                    if (key === 'Date') {
+                        ret[key] = value;
+                    }
+                    else {
+                        if (value === 0) {
+                            value = Math.max(lastValue, 1);
+                        }
+                        ret[key] = Math.min(lastValue / value, 2) * 100 - 100;
+                    }
+                });
+
+                return ret;
+            });
+
+            MG.data_graphic({
+                title: "Departure city of international flights arriving in New Zealand – Month to month evolution",
+                description: "",
+                data: monthToMonthChange,
+                full_width: true,
+                height: 500,
+                area: false,
+                aggregate_rollover: false,
+                target: '#inbound-rate',
+                x_accessor: 'Date',
+                y_accessor: 'TOTAL ALL OVERSEAS PORTS'
+            });
         });
 
 })(window.MG, window.jQuery, window.d3);
