c965c062d6b3b87e364102c36aca1a534c1d32f4
Thibaud Colas <thibaudcolas@gmail.com>
2018-01-22 12:00:24 +0200

    Remove Bower in favor of npm

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,37 +1,16 @@
-(function() {
-  'use strict';
+'use strict';
 
-  require('./analytics')();
+require('./analytics')();
 
-  const domready = require('../vendor/domready/ready');
-  const fitText = require('../vendor/fittext/fittext');
-  const smoothScroll = require('../vendor/smooth-scroll.js/src/js/smooth-scroll');
-  const decode = require('./obfuscate').decode;
+const fitText = require('./vendor/fittext');
+const decode = require('./obfuscate').decode;
 
-  const encodedMail = [
-    116,
-    105,
-    107,
-    101,
-    68,
-    121,
-    110,
-    112,
-    106,
-    55,
-    119,
-    112,
-  ];
+const encodedMail = [116, 105, 107, 101, 68, 121, 110, 112, 106, 55, 119, 112];
 
-  domready(function() {
-    fitText(document.getElementById('main-title'), 0.7, {
-      minFontSize: '35px',
-      maxFontSize: '45px',
-    });
+fitText(document.getElementById('main-title'), 0.7, {
+  minFontSize: '35px',
+  maxFontSize: '45px',
+});
 
-    document.getElementsByClassName('js-email-replace')[0].href =
-      'mailto:' + decode(encodedMail);
-
-    smoothScroll.init({ speed: 300 });
-  });
-})();
+document.getElementsByClassName('js-email-replace')[0].href =
+  'mailto:' + decode(encodedMail);
