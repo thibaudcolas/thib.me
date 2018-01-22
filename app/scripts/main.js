79780970e05f8bf8d244088269392357b0426da6
Thibaud Colas <thibaudcolas@gmail.com>
2018-01-22 11:51:43 +0200

    Remove Hotjar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,20 +2,36 @@
   'use strict';
 
   require('./analytics')();
-  require('./hotjar')();
 
   const domready = require('../vendor/domready/ready');
   const fitText = require('../vendor/fittext/fittext');
   const smoothScroll = require('../vendor/smooth-scroll.js/src/js/smooth-scroll');
   const decode = require('./obfuscate').decode;
 
-  const encodedMail = [116, 105, 107, 101, 68, 121, 110, 112, 106, 55, 119, 112];
+  const encodedMail = [
+    116,
+    105,
+    107,
+    101,
+    68,
+    121,
+    110,
+    112,
+    106,
+    55,
+    119,
+    112,
+  ];
 
   domready(function() {
-    fitText(document.getElementById('main-title'), 0.7, {minFontSize: '35px', maxFontSize: '45px'});
+    fitText(document.getElementById('main-title'), 0.7, {
+      minFontSize: '35px',
+      maxFontSize: '45px',
+    });
 
-    document.getElementsByClassName('js-email-replace')[0].href = 'mailto:' + decode(encodedMail);
+    document.getElementsByClassName('js-email-replace')[0].href =
+      'mailto:' + decode(encodedMail);
 
-    smoothScroll.init({speed: 300});
+    smoothScroll.init({ speed: 300 });
   });
 })();
