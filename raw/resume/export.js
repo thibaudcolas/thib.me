78f1a2e8a9811c49fdf053538dff444ebcc3ad35
Thibaud Colas <thibaudcolas@gmail.com>
2018-01-25 16:47:18 +0200

    Update resume build to integrate with site build

--- raw/resume/export.js
+++ raw/resume/export.js
@@ -1,11 +1,6 @@
-const fs = require("fs");
-const path = require("path");
-const eloquent = require("jsonresume-theme-eloquent");
+const theme = require("jsonresume-theme-eloquent");
 
-const resumeRaw = fs.readFileSync(path.join(__dirname, "/resume.json"));
-const resumeJSON = JSON.parse(resumeRaw, "utf-8");
-const resumeHTML = eloquent.render(resumeJSON);
+const resume = require("./thibaud-resume.json");
+const html = theme.render(resume);
 
-fs.writeFile(path.join(__dirname, "/resume.html"), resumeHTML, function(err) {
-  console.log(err || "Resume exported to resume.html");
-});
+process.stdout.write(html);
