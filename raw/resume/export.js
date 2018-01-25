59a108406daad62733c9bfccef41b9e0459a6888
Thibaud Colas <thibaudcolas@gmail.com>
2018-01-25 18:09:49 +0200

    Use GA on resume

--- raw/resume/export.js
+++ raw/resume/export.js
@@ -1,6 +1,6 @@
 const theme = require("jsonresume-theme-eloquent");
 
-const resume = require("./thibaud-resume.json");
+const resume = require("./resume.json");
 const html = theme.render(resume);
 
 process.stdout.write(html);
