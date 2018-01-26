6b8c286b8e19367d8512a038a31fde2556d75071
Thibaud Colas <thibaudcolas@gmail.com>
2018-01-26 20:29:49 +0200

    Automatically inline portrait image in resume on build

--- raw/resume/export.js
+++ raw/resume/export.js
@@ -1,6 +1,15 @@
+const path = require("path");
+const fs = require("fs");
 const theme = require("jsonresume-theme-eloquent");
 
 const resume = require("./resume.json");
+
+const picturePath = path.join(__dirname, "portrait-thibaud-256x256.jpg");
+const picture = fs.readFileSync(picturePath, "binary");
+const buffer = new Buffer(picture, "binary");
+
+resume.basics.picture = `data:image/jpeg;base64,${buffer.toString("base64")}`;
+
 const html = theme.render(resume);
 
 process.stdout.write(html);
